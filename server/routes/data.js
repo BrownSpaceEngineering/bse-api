ae43f7c38bc2aa4f5723182c655ac0d526f2aafc
Iamjaredlee <jared_lee@brown.edu>
2018-05-03 16:02:44 -0400

    Sorting on data routes

--- server/routes/data.js
+++ server/routes/data.js
@@ -7,6 +7,8 @@ Get all data
 */
 router.get('/', function (req, res, next) {
 	Data.find()
+	.sort('+created')
+	.exec()
 	.then(data => {
 		res.json(data);
 	})
@@ -22,7 +24,10 @@ router.get('/attitude', function (req, res, next) {
   var ATTITUDE = 'ATTITUDE';
   Data.find({
     data_type: ATTITUDE
-  }).then(attitudes => {
+  })
+  .sort('+created')
+  .exec()
+  .then(attitudes => {
     if (attitudes.length) {
       res.json(attitudes);
     } else {
@@ -41,7 +46,10 @@ router.get('/flash-burst', function (req, res, next) {
   var FLASH_BURST = 'FLASH_BURST';
   Data.find({
     data_type: FLASH_BURST
-  }).then(flashBursts => {
+  })
+  .sort('+created')
+  .exec()
+  .then(flashBursts => {
     if (flashBursts.length) {
       res.json(flashBursts);
     } else {
@@ -60,7 +68,10 @@ router.get('/flash-comparison', function (req, res, next) {
   var FLASH_CMP = 'FLASH_CMP';
   Data.find({
     data_type: FLASH_CMP
-  }).then(flashComps => {
+  })
+  .sort('+created')
+  .exec()
+  .then(flashComps => {
     if (flashComps.length) {
       res.json(flashComps);
     } else {
@@ -79,7 +90,10 @@ router.get('/idle', function (req, res, next) {
   var IDLE = 'IDLE';
   Data.find({
     data_type: IDLE
-  }).then(idleData => {
+  })
+  .sort('+created')
+  .exec()
+  .then(idleData => {
     if (idleData.length) {
       res.json(idleData);
     } else {
@@ -98,7 +112,10 @@ router.get('/low-power', function (req, res, next) {
   var LOW_POWER = 'LOW_POWER';
   Data.find({
     data_type: LOW_POWER
-  }).then(lowPower => {
+  })
+  .sort('+created')
+  .exec()
+  .then(lowPower => {
     if (lowPower.length) {
       res.json(lowPower);
     } else {
