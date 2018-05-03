6b010fed27080a5a63d21c9cfbaf6dbcb6fa85a9
Jason Wang <jason@wangjason.com>
2018-05-03 17:59:01 -0400

    added latest transmission and query routes

--- server/routes/transmission.js
+++ server/routes/transmission.js
@@ -1,18 +1,72 @@
 var router = require('express').Router();
 var Transmission = require('../db/models/transmission');
 
+/*
+  Req Query has the following possible parameters
+  end_date: Date, default None
+  start_date: Date, default None
+*/
 router.get('/', function (req, res, next) {
-  Transmission.find()
-  .populate('data')
-  .populate('error_codes')
-  .populate('current_info')
-  .exec()
-  .then(transmissions => {
-    res.json(transmissions);
-  })
-  .catch(err => {
+  try {
+    var query = Transmission.find().populate('data').populate('error_codes').populate('current_info')
+    // Check if end date property exists
+    if (req.query.end_date) {
+      query = query.where({
+        created: {
+          $lte: new Date(req.query.end_date)  // created property is less than that date
+        }
+      })
+    }
+
+    // Check if start date property exists
+    if (req.query.start_date) {
+      query = query.where({
+        created: {
+          $gte: new Date(req.query.start_date)  // created property is greater than that date
+        }
+      })
+    }
+
+    query = query.sort('+created') // ascending order
+
+    query
+    .exec()
+    .then(transmissions => {
+      res.json(transmissions);
+    })
+    .catch(err => {
+      next(err);
+    })
+  } catch (err) {
+    err.status = 400;
     next(err);
-  })
+  }
+})
+
+/*
+  Request Query
+  limit: integer, default everything
+*/
+router.get('/latest', function (req, res, next) {
+  try {
+    var query = Transmission.find().sort('-created');
+
+    if (req.query.limit) {
+      query = query.limit(+req.query.limit) // cast to number
+    }
+
+    query.exec()
+    .then(codes => {
+      res.json(codes);
+    })
+    .catch(err => {
+      next(err);
+    })
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+
 })
 
 router.get('/:cuid', function (req, res, next) {
