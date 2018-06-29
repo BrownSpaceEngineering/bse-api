0a2d2075e20acdd5d5d59e583a84acfbc2899a3b
Tyler Fox <tyler_fox@brown.edu>
2018-06-29 16:26:44 -0400

    added latest_single for signals

--- server/routes/signals.js
+++ server/routes/signals.js
@@ -130,52 +130,52 @@ router.get('/latest', function (req, res, next) {
   		dataQuery
 	]).then(([ currentInfos, data ]) => {		
 		if (req.query.fields) {			
-	    	var toReturn = {};
+    	var toReturn = {};
 
-	    	var fields = req.query.fields.split(',');
-	    	currentInfos.forEach(currentInfo => {
-	    		fields.forEach(field => {
-		        	if (!(field in toReturn)) {
-		        		toReturn[field] = [];
-		        	}
-		        	if (field in currentInfo) {
-		        		var curEntry = {
-            				timestamp: currentInfo.created.getTime(),
-	            			value: currentInfo[field]
-          				};
-          				toReturn[field].push(curEntry);
-		        	}
-	        	});
-	    	});	    	
-	    	data.forEach(datum => {
-	    		fields.forEach(field => {
-		        	if (!(field in toReturn)) {
-		        		toReturn[field] = [];
-		        	}
-		        	if (field in datum.payload) {
-		        		var curEntry = {
-	            			timestamp: datum.created.getTime(),
-            				value: datum.payload[field]
-          				};
-          				toReturn[field].push(curEntry);
-          			}
-	        	});
-	    	});
-	    	//sort arrays by timestamp
-	    	for (var field in toReturn) {
-	    		toReturn[field].sort(function(a,b) {
+    	var fields = req.query.fields.split(',');
+    	currentInfos.forEach(currentInfo => {
+    		fields.forEach(field => {
+        	if (!(field in toReturn)) {
+        		toReturn[field] = [];
+        	}
+        	if (field in currentInfo) {
+        		var curEntry = {
+        				timestamp: currentInfo.created.getTime(),
+          			value: currentInfo[field]
+      				};
+      				toReturn[field].push(curEntry);
+        	}
+        });
+    	});	    	
+    	data.forEach(datum => {
+    		fields.forEach(field => {
+        	if (!(field in toReturn)) {
+        		toReturn[field] = [];
+        	}
+        	if (field in datum.payload) {
+        		var curEntry = {
+        			timestamp: datum.created.getTime(),
+      				value: datum.payload[field]
+    				};
+    				toReturn[field].push(curEntry);
+    			}
+        });
+    	});
+    	//sort arrays by timestamp
+    	for (var field in toReturn) {
+    		toReturn[field].sort(function(a,b) {
 					return new Date(b.timestamp) - new Date(a.timestamp);
-				});
-				if (req.query.limit) {
-					toReturn[field] = toReturn[field].slice(0, req.query.limit);					
-				}
-				toReturn[field] = separateArray(toReturn[field]);
-	    	}
-	    	res.json(toReturn);
-        } else {        	        	
-        	res.statusMessage = "Must specify at least one signal field";
-    		res.status(400).end();        
-        }
+			  });
+ 			 if (req.query.limit) {
+				 toReturn[field] = toReturn[field].slice(0, req.query.limit);
+			  }
+			 toReturn[field] = separateArray(toReturn[field]);
+    	}
+    	res.json(toReturn);
+    } else {        	        	
+      res.statusMessage = "Must specify at least one signal field";
+  		res.status(400).end();        
+    }
 	})
     .catch(err => {
       next(err);
@@ -186,19 +186,67 @@ router.get('/latest', function (req, res, next) {
   }
 })
 
-router.get('/:transmissionCuid', function (req, res, next) {
+/*
+  Request Query  
+  fields: comma delimited String, default everything
+*/
+router.get('/latest_single', function (req, res, next) {
   try {
-    var transmissionCuid = req.params.transmissionCuid;
+    var curInfoQuery = CurrentInfo.find().sort('-created');
+    var dataQuery = Data.find().sort('-created');    
 
-    CurrentInfo.findOne({
-      transmission_cuid: transmissionCuid
-    }).then(currentInfo => {
-      if (currentInfo) {
-        res.json(currentInfo);
-      } else {
-        res.status(400).send('Current Info not found');
+    /*if (req.query.limit) {
+      query = query.limit(+req.query.limit) // cast to number
+    }*/    
+    Promise.all([
+      curInfoQuery,
+      dataQuery
+  ]).then(([ currentInfos, data ]) => {   
+    if (req.query.fields) {     
+      var toReturn = {};
+
+      var fields = req.query.fields.split(',');
+      currentInfos.forEach(currentInfo => {
+        fields.forEach(field => {
+          if (!(field in toReturn)) {
+            toReturn[field] = [];
+          }
+          if (field in currentInfo) {
+            var curEntry = {
+              timestamp: currentInfo.created.getTime(),
+              value: currentInfo[field]
+            };
+            toReturn[field].push(curEntry);
+          }
+        });
+      });       
+      data.forEach(datum => {
+        fields.forEach(field => {
+          if (!(field in toReturn)) {
+            toReturn[field] = [];
+          }
+          if (field in datum.payload) {
+            var curEntry = {
+              timestamp: datum.created.getTime(),
+              value: datum.payload[field]
+            };
+              toReturn[field].push(curEntry);
+          }
+        });
+      });
+      //sort arrays by timestamp
+      for (var field in toReturn) {
+        toReturn[field].sort(function(a,b) {
+          return new Date(b.timestamp) - new Date(a.timestamp);
+        });        
+        toReturn[field] = toReturn[field][0];
       }
-    })
+      res.json(toReturn);
+    } else {                    
+      res.statusMessage = "Must specify at least one signal field";
+      res.status(400).end();        
+    }
+  })
     .catch(err => {
       next(err);
     })
@@ -206,7 +254,6 @@ router.get('/:transmissionCuid', function (req, res, next) {
     err.status = 400;
     next(err);
   }
-
 })
 
 module.exports = router;
