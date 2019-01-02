d975f06008e8f02371f25f8089cc8552d217bf9c
Mckenna Cisler <mckennacisler@gmail.com>
2019-01-02 11:49:44 -0500

    Update DB update script

--- utils/update_transmission_field.js
+++ utils/update_transmission_field.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var routes = require('../server/routes'); // needed for transmission
 var Transmission = require('../server/db/models/transmission');
 
-ACTUALLY_UPDATE = true
+ACTUALLY_UPDATE = false
 
 connectDb
 .then(() => {
@@ -30,20 +30,21 @@ function run() {
     var transmissionUpdates = transmissions.map(tx => {
       console.log(`Updating transmission ${tx._id} with timestamp ${tx.preamble.timestamp}`)
 
-      // updates sources in DB BEFORE WE STARTED GETTING DATA FROM NEW APPS
-      for (var i = 0; i < tx.station_names.length; i++) {
-        tx.sources.push("equistation");
-      }
-
-      if (ACTUALLY_UPDATE) {
-        tx.save()
-          .then(updated => {
-            console.log(`${tx._id}: updated transmission`);
-          })
-          .catch(err => {
-            console.error(`${tx._id}: error updating transmission: ${err}`);
-          });
-      }
+      // updated sources in DB BEFORE WE STARTED GETTING DATA FROM NEW APPS
+      // (DON'T RUN THIS AGAIN)
+      // for (var i = 0; i < tx.station_names.length; i++) {
+      //   tx.sources.push("equistation");
+      // }
+      //
+      // if (ACTUALLY_UPDATE) {
+      //   tx.save()
+      //     .then(updated => {
+      //       console.log(`${tx._id}: updated transmission`);
+      //     })
+      //     .catch(err => {
+      //       console.error(`${tx._id}: error updating transmission: ${err}`);
+      //     });
+      // }
   });
   })
   .catch(err => {
